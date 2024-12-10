ff8ff4ec6cf1a56b2d02b9786cd22478d54e2693
Anuchit Chalothorn <anoochit@gmail.com>
2024-12-10 10:06:50 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/multiple/select/gridview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.multiple_select_gridview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:multiple_select_gridview/app/app.dart';
+import 'package:multiple_select_gridview/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
